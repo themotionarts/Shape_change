ff77e320022f12b7299e9935f004666ddba4dc59
Motion Arts <motionstudios4@gmail.com>
2020-06-16 12:40:16 +0300

    no message

---
+++ shape_change_game/Assets/scripts/Hit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public Rigidbody rb;
+    //public MeshCollider mc;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(canvas, new Vector3 (0, 0, 0), Quaternion.identity);//add game over to screen
+        Time.timeScale = 0f;
+    }
+}
