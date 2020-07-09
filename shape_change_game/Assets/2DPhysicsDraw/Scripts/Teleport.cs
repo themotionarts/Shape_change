edaeac4718cf1c40c149ef2ff4e219d76398ce70
Motion Arts <motionstudios4@gmail.com>
2020-07-09 17:31:36 +0300

    commit message

---
+++ shape_change_game/Assets/2DPhysicsDraw/Scripts/Teleport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject endPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+    }
+
+    public GameObject carrier;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        carrier.transform.position = this.transform.position;
+        other.transform.parent = carrier.transform;
+
+        carrier.gameObject.transform.position = endPos.transform.position;
+        other.transform.parent = null;
+    }
+
+}
