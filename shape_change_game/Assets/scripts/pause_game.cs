1e2017b3168238a4016fcc1cd7f85d8144b20b4a
Motion Arts <motionstudios4@gmail.com>
2020-07-09 17:27:52 +0300

    no message

---
+++ shape_change_game/Assets/scripts/pause_game.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pause_game : MonoBehaviour
+{
+    public GameObject pause;
+    public GameObject _pauseButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void pause_button()
+    {
+        pause.SetActive(true);
+        _pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void resume()
+    {
+        pause.SetActive(false);
+        _pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    public void quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName: "Main Menu");
+    }
+}
